91de77e6c173b9c120de92d1cd7b4a66f2d3516d
Mohammed Soliman <m.youssef.soliman@gmail.com>
2023-03-02 16:50:49 +0200

    live test

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -31,26 +31,73 @@ export function useShoppingCart() {
 export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
   const [isOpen, setIsOpen] = useState(false);
   // TODO: fix the type of cartItems
-  const [cartItems, setCartItems] = useLocalStorage('shopping-cart', []);
+  const [cartItems, setCartItems] = useLocalStorage<CartItem[]>(
+    'shopping-cart',
+    []
+  );
 
   const openCart = () => setIsOpen(true);
   const closeCart = () => setIsOpen(false);
 
   // TODO: calculate cart quantity
-  const cartQuantity = 0;
+  const cartQuantity = cartItems.reduce((pervQuantity, currentItem) => {
+    return pervQuantity + currentItem.quantity;
+  }, 0);
   // TODO: calculate cart total
-  const cartTotalAmount = 0;
+  const cartTotalAmount = cartItems.reduce((pervTotal, currentItem) => {
+    const itemTotal =
+      storeItems.find(storedItem => storedItem.id === currentItem.id)?.price ||
+      0;
+    return pervTotal + itemTotal;
+  }, 0);
 
   // TODO: implement getItemQuantity
   const getItemQuantity = (id: number) => {
-    return 0;
+    // cartItems.reduce((first, next) => {
+    //   return first.
+    // })
+    return cartItems.find(item => item.id === id)?.quantity || 0;
   };
   // TODO: implement increaseCartQuantity
-  function increaseCartQuantity(id: number) {}
+  function increaseCartQuantity(id: number) {
+    let foundItem = cartItems.find(item => item.id === id);
+    const foundItemIndex = cartItems.findIndex(item => item.id === id);
+    let updatedItems = [...cartItems];
+    if (!foundItem || !foundItemIndex) {
+      foundItem = {
+        id,
+        quantity: 1,
+      };
+      updatedItems.push(foundItem);
+    }
+    if (foundItem) {
+      foundItem.quantity += 1;
+      updatedItems[foundItemIndex] = foundItem;
+    }
+    setCartItems(updatedItems);
+  }
   // TODO: implement decreaseCartQuantity
-  function decreaseCartQuantity(id: number) {}
+  function decreaseCartQuantity(id: number) {
+    let foundItem = cartItems.find(item => item.id === id);
+    const foundItemIndex = cartItems.findIndex(item => item.id === id);
+    let updatedItems = [...cartItems];
+    if (!foundItem || !foundItemIndex) {
+      updatedItems = updatedItems.filter(item => item.id !== id);
+    }
+    if (foundItem) {
+      if (foundItem.quantity < 1) {
+        updatedItems = updatedItems.filter(item => item.id !== id);
+      } else {
+        foundItem.quantity -= 1;
+        updatedItems[foundItemIndex] = foundItem;
+      }
+    }
+    setCartItems(updatedItems);
+  }
   // TODO: implement removeFromCart
-  function removeFromCart(id: number) {}
+  function removeFromCart(id: number) {
+    setCartItems(prevItems => prevItems.filter(item => item.id !== id));
+  }
 
   return (
     <ShoppingCartContext.Provider
