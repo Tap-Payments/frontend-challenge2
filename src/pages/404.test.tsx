df6d212e245f4f7968d9142e68a90710b979f550
Mohammed Soliman <m.youssef.soliman@gmail.com>
2023-03-02 16:51:37 +0200

    updated tests

--- src/pages/404.test.tsx
+++ src/pages/404.test.tsx
@@ -1,5 +1,5 @@
 import { describe, test, expect, vi } from 'vitest';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import NotFoundPage from './404';
 
 describe('<App />', () => {
