17a5892db547d0f0244b8f529e14ba3337c1498c
Safei Ashraf <safei.ashraf@gmail.com>
2023-03-27 15:42:49 +0200

    feat: challenge without qty

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -31,26 +31,60 @@ export function useShoppingCart() {
 export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
   const [isOpen, setIsOpen] = useState(false);
   // TODO: fix the type of cartItems
-  const [cartItems, setCartItems] = useLocalStorage('shopping-cart', []);
+  const [cartItems, setCartItems] = useLocalStorage<CartItem[]>('shopping-cart', []);
 
   const openCart = () => setIsOpen(true);
   const closeCart = () => setIsOpen(false);
 
   // TODO: calculate cart quantity
-  const cartQuantity = 0;
+  const cartQuantity = (cartItems: CartItem[]) => {
+    let quantity = 0;
+    cartItems.forEach(cartItem => {
+      const itemPrice = storeItems.filter(storeItem => storeItem.id === cartItem.id)[0].price;
+      quantity += itemPrice;
+    })
+    return quantity
+  };;
   // TODO: calculate cart total
-  const cartTotalAmount = 0;
+  const cartTotalAmount = (cartItems: CartItem[]) => {
+    let total = 0;
+    cartItems.forEach(cartItem => {
+      const itemPrice = storeItems.filter(storeItem => storeItem.id === cartItem.id)[0].price;
+      total += itemPrice;
+    })
+    return total
+  };
+
 
   // TODO: implement getItemQuantity
   const getItemQuantity = (id: number) => {
-    return 0;
+    const countInCart = cartItems.filter(elem => elem.id === id)[0].quantity;
+    return countInCart;
   };
   // TODO: implement increaseCartQuantity
-  function increaseCartQuantity(id: number) {}
+  function increaseCartQuantity(id: number) {
+    const productIndex = cartItems.findIndex(elem => elem.id === id);
+    const productToUpdate: CartItem = { ...cartItems[productIndex], quantity: cartItems[productIndex].quantity += 1}
+    const updatedCart = cartItems.slice();
+    updatedCart.splice(productIndex, 1, productToUpdate);
+    setCartItems(updatedCart)
+  }
   // TODO: implement decreaseCartQuantity
-  function decreaseCartQuantity(id: number) {}
+  function decreaseCartQuantity(id: number) {
+    const productIndex = cartItems.findIndex(elem => elem.id === id);
+    const productToUpdate: CartItem = { ...cartItems[productIndex], quantity: cartItems[productIndex].quantity -= 1}
+    const updatedCart = cartItems.slice();
+    updatedCart.splice(productIndex, 1, productToUpdate);
+    setCartItems(updatedCart)
+  }
   // TODO: implement removeFromCart
-  function removeFromCart(id: number) {}
+  function removeFromCart(id: number) {
+    const productIndex = cartItems.findIndex(elem => elem.id === id);
+    const productToUpdate: CartItem = { ...cartItems[productIndex], quantity: cartItems[productIndex].quantity = 0}
+    const updatedCart = cartItems.slice();
+    updatedCart.splice(productIndex, 1, productToUpdate);
+    setCartItems(updatedCart)
+  }
 
   return (
     <ShoppingCartContext.Provider
